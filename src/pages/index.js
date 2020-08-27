b68694f3ae3d848a7eb82fdc7ebd631c628d4ba2
C. Evan Carlisle <cevancarlisle@gmail.com>
2020-08-26 22:09:50 -0400

    check change in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Haiiiii</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>boody boody boody boody</p>
+    <p>have a great job</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
