1923deb18d49576f5132103303d7a4aa6b34d1b8
C. Evan Carlisle <cevancarlisle@gmail.com>
2020-09-18 09:32:58 -0400

    source page changed (?):

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,25 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 const Contact = () => {
+  function hoverOver(event) {
+    event.target.className = "animate__animated animate__fadeOut"
+  }
+
+  function hoverOff(event) {
+    event.target.className = "animate__animated animate__fadeIn"
+  }
   return (
     <Layout>
       <SEO title="contact" />
       <h1 style={{ marginTop: "5rem", fontSize: "16px" }}>
         for all inquiries:{" "}
-        <a href="mailto: phytin4@gmail.com">phytin4@gmail.com</a>
+        <a
+          onMouseOver={hoverOver}
+          onMouseOut={hoverOff}
+          href="mailto: phytin4@gmail.com"
+        >
+          phytin4@gmail.com
+        </a>
       </h1>
     </Layout>
   )
